aaa62381798f4d309c653dad9e0dd677b6f9ebcd
José Eudes <jeudes894@gmail.com>
2025-08-04 09:57:19 -0300

    criando a models

--- pedidos/models.py
+++ pedidos/models.py
@@ -1,3 +1,33 @@
+# models.py
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    USER_TYPE_CHOICES = (
+        ("cliente", "Cliente"),
+        ("restaurante", "Restaurante"),
+    )
+    user_type = models.CharField(
+        max_length=20,
+        choices=USER_TYPE_CHOICES,
+        default="cliente",
+    )
+
+    # Adicione estes campos para resolver o erro
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='pedidos_user_set',
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        related_query_name='pedidos_user',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='pedidos_user_permissions_set',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_query_name='pedidos_user_permission',
+    )
+
+    def __str__(self):
+        return self.username
